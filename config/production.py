ba4c885700f450cc70a73cd566b8867e52a9306f
DESKTOP-0M1SFST\user <kay061476@gmail.com>
2022-11-02 20:57:43 +0900

    11.02(4)_flask

--- config/production.py
+++ config/production.py
@@ -3,11 +3,12 @@ from config.default import *
 
 load_dotenv(os.path.join(BASE_DIR, '.env'))
 
-SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
-    user=os.getenv('viking'),
-    pw=os.getenv('dkagh1.'),
-    url=os.getenv('localhost'),
-    db=os.getenv('board'))
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}:{port}/{db}'.format(
+    user=os.getenv('DB_USER'),
+    pw=os.getenv('DB_PASSWORD'),
+    url=os.getenv('DB_HOST'),
+    port=os.getenv('DB_PORT'),
+    db=os.getenv('DB_NAME'))
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'\xe0\xd3x9\xfe\xbb\x14J\xd8\xf6\xff\xae\x19\xed\xac\x04'
