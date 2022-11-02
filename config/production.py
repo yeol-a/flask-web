733e93575d0d1e2091e1651a628b2de2fa5aaf68
DESKTOP-0M1SFST\user <kay061476@gmail.com>
2022-11-02 19:13:26 +0900

    1102(3)_flask

--- config/production.py
+++ config/production.py
@@ -8,3 +8,6 @@ SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
     pw=os.getenv('dkagh1.'),
     url=os.getenv('localhost'),
     db=os.getenv('board'))
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SECRET_KEY = b'\xe0\xd3x9\xfe\xbb\x14J\xd8\xf6\xff\xae\x19\xed\xac\x04'
