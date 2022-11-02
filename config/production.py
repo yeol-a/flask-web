8edd713fa7bb17cdc83b3563e69ec30fc6aefc96
DESKTOP-0M1SFST\user <kay061476@gmail.com>
2022-11-02 16:29:11 +0900

    flask_1102

---
+++ config/production.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+
+load_dotenv(os.path.join(BASE_DIR, '.env'))
+
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
+    user=os.getenv('DB_USER'),
+    pw=os.getenv('DB_PASSWORD'),
+    url=os.getenv('DB_HOST'),
+    db=os.getenv('DB_NAME'))
